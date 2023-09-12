ee45fed149e130b7045252f9796389f8068d36d2
Michael Alao <contactleomax@gmail.com>
2023-09-12 11:48:42 +0100

    Worked on post and put

--- PokemonReviewApp/Interfaces/ICategoryRepository.cs
+++ PokemonReviewApp/Interfaces/ICategoryRepository.cs
@@ -8,6 +8,7 @@ namespace PokemonReviewApp.Interfaces
         ICollection<Pokemon> GetPokemonByCategory(int categoryId);
         bool CategoryExists(int id);
         bool CreateCategory(Category category);
+        bool UpdateCategory(Category category);
         bool Save();
     }
 }
--- PokemonReviewApp/Interfaces/IPokemonRepository.cs
+++ PokemonReviewApp/Interfaces/IPokemonRepository.cs
@@ -13,6 +13,8 @@ namespace PokemonReviewApp.Interfaces
         decimal GetPokemonRating(int pokeId);
 
         bool PokemonExists(int pokeId);
+        bool CreatePokemon(int ownerId,int categoryId,Pokemon pokemon);
+        bool Save();
 
     }
 }
